cfe81c38fe71fd15096e100fe3ad895e8f87b18a
KevinBanegasUNITEC <kevinandre0504@unitec.edu>
2024-07-12 16:21:27 -0600

    Interviewer saved, in GCP

--- src/ConfigurationModule/createInterviewer/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Interviewer
-
-class InterviewerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Interviewer
-        fields = '__all__'
--- src/ConfigurationModule/createInterviewer/views.py
+++ src/ConfigurationModule/createInterviewer/views.py
@@ -1,9 +1,63 @@
+from django.http import JsonResponse
 from django.shortcuts import render
-from rest_framework import generics
-from .models import Interviewer
-from .serializer import InterviewerSerializer
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+from django.core.files.storage import default_storage
+from google.cloud import storage
 
-class InterviewerListCreate(generics.ListCreateAPIView):
-    queryset = Interviewer.objects.all()
-    serializer_class = InterviewerSerializer
+import os
+db = settings.MONGO_DB
+bucket = storage.Client().get_bucket('cactusbucket')
 
+@csrf_exempt
+def createInterviewer(request):
+    if request.method == 'POST':
+        body = request.POST
+        filename = None
+        if(request.FILES):
+            image_file = request.FILES['interviewerProfilePicture']
+            filename = f'pfp/{body.get('surveyId')}/'+image_file.name
+            blob = bucket.blob(filename)
+            blob.upload_from_file(image_file)
+        data = {
+            'interviewerName':body.get('interviewerName'),
+            'interviewerProfilePicture':filename,
+            'interviewerTone':body.get('interviewerTone'),
+            'interviewerGreeting':body.get('interviewerGreeting'),
+            'importantObservation':body.get('importantObservation'),
+            'surveyId':body.get('surveyId')
+        }
+        post = db['Interviewer'].insert_one(data)
+        
+        return JsonResponse({
+            'status': 'success',
+            'interviewer_id': str(post.inserted_id)
+        })
+    return JsonResponse({'error': 'Invalid request method'})
+
+@csrf_exempt
+def getInterviewer(request):
+    if request.method == 'POST':
+        interviewer = db['Interviewer'].find_one({'studyId': request.POST['studyId']})
+        pfp = interviewer.get('interviewerProfilePicture')
+        blob = bucket.blob(pfp)
+        return JsonResponse({
+            'interviewers': [
+                {
+                    'interviewer_id': str(interviewer['_id']),
+                    'interviewerName': interviewer['interviewerName'],
+                    'interviewerProfilePicture': pfp,
+                    'interviewerTone': interviewer['interviewerTone'],
+                    'interviewerGreeting': interviewer['interviewerGreeting'],
+                    'studyId': interviewer['studyId']
+                } 
+            ]
+        })
+    return JsonResponse({'error': 'Invalid request method'})
+
+def getInterviewerPfp(request):
+    if request.method == 'POST':
+        interviewer = db['Interviewer'].find_one({'studyId': request.POST['studyId']})
+        pfp = interviewer.get('interviewerProfilePicture')
+        blob = bucket.blob(pfp)
+    return JsonResponse({'error': 'Invalid request method'})
