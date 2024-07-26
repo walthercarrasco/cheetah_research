55eaf3660122516cf6f2d01085810ee4f31e86f7
Walther Carrasco <walthercarrasco16@gmail.com>
2024-07-26 14:33:51 -0600

    obtiene los resumenes

--- src/ConfigurationModule/ConfigurationModule/urls.py
+++ src/ConfigurationModule/ConfigurationModule/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
     path("api/v1/dj-rest-auth/", include("dj_rest_auth.urls")),
     path('', include('list_studies.urls'), name='list_studies'),
+    path('', include('summaries.urls'), name='summaries'),
 ]
