aaa0d031c9014793f6630d449df7375772752003
Alberth Godoy 12111345 <131223611+AlexVila0204@users.noreply.github.com>
2024-08-08 22:26:43 -0600

    Update views.py
    fixed with deleteStudy, now delete S3 sources

--- src/ConfigurationModule/deleteStudy/views.py
+++ src/ConfigurationModule/deleteStudy/views.py
@@ -5,11 +5,13 @@ import urllib.request
 from bson import ObjectId
 import json
 import pymongo
+import boto3
 
 db = settings.MONGO_DB
 @csrf_exempt
 def delete_study(request):
     if request.method == 'DELETE':
+        print("DELETE")
         try:
 
             data = json.loads(request.body)
@@ -21,6 +23,40 @@ def delete_study(request):
             study = db['Study'].find_one({'_id': ObjectId(study_id)})
             if not study:
                 return JsonResponse({'status': 'error', 'message': 'Study not found.'}, status=404)
+
+            s3 = boto3.client('s3',
+                                aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+                                aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+                                region_name='us-east-1')
+            bucket_name = settings.BUCKET_NAME
+            bucket_name2 = settings.BUCKET_DATA
+            s3_key_prefixprofile = f"pfp/{study_id}"
+            s3_key_prefiximagesd = f"images/{study_id}"
+            s3_key_prefixanalysis = f"analysis/{study_id}"
+            s3_key_prefixsurveys = f"surveys/{study_id}"
+            responseprofile = s3.list_objects_v2(Bucket=bucket_name, Prefix=s3_key_prefixprofile)
+            responseimages = s3.list_objects_v2(Bucket=bucket_name, Prefix=s3_key_prefiximagesd)
+            responseanalysis = s3.list_objects_v2(Bucket=bucket_name2, Prefix=s3_key_prefixanalysis)
+            responsesurveys = s3.list_objects_v2(Bucket=bucket_name2, Prefix=s3_key_prefixsurveys)
+            #elimina las imagenes de los perfiles en interviewer
+            if 'Contents' in responseprofile:
+                for obj in responseprofile['Contents']:
+                    s3.delete_object(Bucket=bucket_name, Key=obj['Key'])
+            #elimina las imagenes del S3
+            if 'Contents' in responseimages:
+                for obj in responseimages['Contents']:
+                    s3.delete_object(Bucket=bucket_name, Key=obj['Key'])
+            #elimina los archivos de analisis
+            if 'Contents' in responseanalysis:
+                for obj in responseanalysis['Contents']:
+                    s3.delete_object(Bucket=bucket_name2, Key=obj['Key'])
+            #elimina los archivos de encuestas
+            if 'Contents' in responsesurveys:
+                for obj in responsesurveys['Contents']:
+                    s3.delete_object(Bucket=bucket_name2, Key=obj['Key'])
+
+
+
 
             db['Study'].delete_one({'_id': ObjectId(study_id)})
 
